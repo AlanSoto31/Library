3e87f20c7a1b75e2364422c5af1ff7a459eab658
thneves <tsneves11@gmail.com>
2021-03-12 17:23:43 -0300

    Create delete card function, and status change

--- script.js
+++ script.js
@@ -13,35 +13,59 @@ function addBookToLibrary(book) {
 
 function printBooks(arr){
   listCon.innerHTML = '';
-  arr.map(item => {
+  arr.map((item, index) => {
 
     let div = document.createElement('div');
     let ul = document.createElement('ul');
     let li1 = document.createElement('li');
     let li2 = document.createElement('li');
     let li3 = document.createElement('li');
-    let li4 = document.createElement('li');
+    let status = document.createElement('button');
+    let deleteBtn = document.createElement('button');
 
-    div.classList.add('card');
+    div.classList.add('card', 'm-5', 'border', 'border-dark', 'border-2', 'text-center');
+    div.setAttribute('id', `book${index}`);
     ul.classList.add('list-group', 'list-group-flush');
     li1.classList.add('list-group-item');
     li2.classList.add('list-group-item');
     li3.classList.add('list-group-item');
-    li4.classList.add('list-group-item');
+    status.classList.add('list-group-item');
+    deleteBtn.classList.add('delete-btn', 'btn-danger');
     
     listCon.appendChild(div);
     div.appendChild(ul);
-
+    div.appendChild(deleteBtn);
     ul.appendChild(li1);
     li1.textContent = item.title;
     ul.appendChild(li2);
     li2.textContent = item.author;
     ul.appendChild(li3);
     li3.textContent = item.pages;
-    ul.appendChild(li4);
-    li4.textContent = item.read;
+    ul.appendChild(status);
+    status.textContent = 'Not read yet!';
+    status.addEventListener('click',() => {
+      if (status.textContent == 'I read it!') {
+        status.textContent = 'Not read yet!'
+      } else {
+        status.textContent = 'I read it!';
+      }
+    })
+
+    deleteBtn.textContent = 'Delete Book'
+    deleteBtn.addEventListener('click', () => {
+      arr.splice(index,1);
+      printBooks(arr);
+    })
   });
 }
+
+const deleteButtons = document.querySelectorAll('.btn-danger');
+
+// deleteBtn.addEventListener('click', () => {
+//     deleteButtons.map((item, index) => {
+//       myLibrary.splice(index, 1)
+//     })
+// })
 
 const book1 = new Book('The Hobbit', 'R.R.', '564', 'no');
 const book2 = new Book('It', 'Stephen King', '365', 'yes');
@@ -49,7 +73,12 @@ const book2 = new Book('It', 'Stephen King', '365', 'yes');
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
+const btn = document.querySelector('#add-btn');
+const form = document.querySelector('form')
 
+btn.addEventListener('click', () => {
+  form.classList.toggle('d-none');
+})
 //DOM Manipulation
 
 let listCon = document.getElementById('book-list-con');
